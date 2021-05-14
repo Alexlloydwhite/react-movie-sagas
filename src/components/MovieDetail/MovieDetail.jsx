644ce04f6b76538ef153c29c8f03f00f218d04e7
Alex White <alexlloydwhite@gmail.com>
2021-05-13 19:59:51 -0500

    movieDetail compo displays info abou the movie

--- src/components/MovieDetail/MovieDetail.jsx
+++ src/components/MovieDetail/MovieDetail.jsx
@@ -1,10 +1,14 @@
 import { useDispatch, useSelector } from "react-redux";
 
 const MovieDetail = () => {
+    const movieClickedDetail = useSelector(store => store.movieClicked);
 
     return (
         <div>
             <h2>details about the movie go here...</h2>
+            {movieClickedDetail.map(movie => {
+                return <h1 key={movie.id}>{movie.description}</h1>
+            })}
         </div>
     );
 }
