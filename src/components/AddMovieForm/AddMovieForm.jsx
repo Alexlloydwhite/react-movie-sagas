ff58ee2b6727f1f5bd76a5dd226bca62d9e58dcf
Alex White <alexlloydwhite@gmail.com>
2021-05-13 20:29:59 -0500

    genres display in selector

--- src/components/AddMovieForm/AddMovieForm.jsx
+++ src/components/AddMovieForm/AddMovieForm.jsx
@@ -5,8 +5,9 @@ const AddMovieForm = () => {
     const [movieTitle, setMovieTitle] = useState('');
     const [movieUrl, setMovieUrl] = useState('');
     const [movieDescription, setMovieDescription] = useState('');
-    const [genre, setGenre] = useState('Genre 1')
+    const [genre, setGenre] = useState('')
     const dispatch = useDispatch();
+    // array of genres from the store
     const genres = useSelector(store => store.genres);
 
     const handleSubmit = (e) => {
@@ -20,32 +21,35 @@ const AddMovieForm = () => {
 
     return (
         <div>
-            {JSON.stringify(genres)}
             <h1>Add a New Movie!</h1>
+            {/* form for submitting new movie */}
             <form onSubmit={handleSubmit}>
+                {/* input takes title */}
                 <input
                     value={movieTitle}
                     placeholder="Movie Title"
                     onChange={(e) => setMovieTitle(e.target.value)}
                 />
+                {/* input takes url */}
                 <input
                     value={movieUrl}
                     placeholder="Movie Url"
                     onChange={(e) => setMovieUrl(e.target.value)}
                 />
+                {/* input takes description */}
                 <input
                     value={movieDescription}
                     placeholder="Movie Description"
                     onChange={(e) => setMovieDescription(e.target.value)}
                 />
+                {/* selector for genre */}
                 <select
                     onChange={(e) => setGenre(e.target.value)}
                 >
-                    <option>Genre 1</option>
-                    <option>Genre 2</option>
-                    <option>Genre 3</option>
-                    <option>Genre 4</option>
-                    <option>Genre 5</option>
+                    {/* maps over array of genres, displays each as an option! */}
+                    {genres.map(genre => {
+                        return <option key={genre.id}>{genre.name}</option>
+                    })}
                 </select>
                 <button type="submit">Submit</button>
             </form>
