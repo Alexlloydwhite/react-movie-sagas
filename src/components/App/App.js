59ce4c32c771b161bebae2a6b6bbd6b9f350ee3e
Alex White <alexlloydwhite@gmail.com>
2021-05-14 16:23:49 -0500

    MUI card and grid init for movie img

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,4 @@
 import {HashRouter as Router, Route } from 'react-router-dom';
-import './App.css';
 import Header from './Header';
 import MovieList from '../MovieList/MovieList';
 import MovieDetail from '../MovieDetail/MovieDetail';
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,45 +1,60 @@
+// IMPORTS FOR REACT FRAMEWORKS!
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router';
-import './MovieList.css'
+// -------------------- MUI -----------------------
+import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Grid from '@material-ui/core/Grid';
+import { Container, makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles(theme => ({
+    img: {
+        cursor: "pointer"
+    }
+}));
 
 function MovieList() {
     const history = useHistory();
     const dispatch = useDispatch();
+    const classes = useStyles();
     const movies = useSelector(store => store.movies);
-
-    useEffect(() => {
-        dispatch({ type: 'FETCH_MOVIES' });
-    }, []);
 
     // Functions fires on click of img poster
     // Grabs the movies ID and sends it to store
     // We will use this to load the movies details
     const handleDetailsView = (id) => {
         history.push(`/details/${id}`)
-        dispatch({ type: 'SET_MOVIE_CLICK', payload: id})
+        dispatch({ type: 'SET_MOVIE_CLICK', payload: id })
     }
 
+    useEffect(() => {
+        dispatch({ type: 'FETCH_MOVIES' });
+    }, []);
+
     return (
-        <main>
-            <h1>MovieList</h1>
-            <section className="movies">
+        <Container>
+            <Typography variant="h2">Movie List</Typography>
+            <br />
+            <Grid container spacing={5}>
                 {movies.map(movie => {
                     return (
-                        <div className="moviecard" key={movie.id} >
-                            <h3>{movie.title}</h3>
+                        <Card key={movie.id} elevation={0} className={classes.root}>
+                            <CardContent>
                                 <img
                                     src={movie.poster}
                                     alt={movie.title}
-                                    className="movie-img"
+                                    className={classes.img}
                                     //  on click of movie poster img fire function to hold ID in redux
                                     onClick={() => handleDetailsView(movie.id)}
                                 />
-                        </div>
+                            </CardContent>
+                        </Card>
                     );
                 })}
-            </section>
-        </main>
+            </Grid>
+        </Container>
 
     );
 }
