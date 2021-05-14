ea9e1c41df27e86a9a26db3be6e9963aef7f9b55
Alex White <alexlloydwhite@gmail.com>
2021-05-13 20:04:38 -0500

    added button on detail compo to take user back to home page

--- src/components/MovieDetail/MovieDetail.jsx
+++ src/components/MovieDetail/MovieDetail.jsx
@@ -1,13 +1,16 @@
 import { useSelector } from "react-redux";
+import { useHistory } from "react-router";
 
 const MovieDetail = () => {
     const movieClickedDetail = useSelector(store => store.movieClicked);
+    const history = useHistory();
 
     return (
         <div>
             {movieClickedDetail.map(movie => {
                 return <h1 key={movie.id}>{movie.description}</h1>
             })}
+            <button onClick={() => history.push('/')}>Back to List</button>
         </div>
     );
 }
