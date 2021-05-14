d4b57124cdfd6fa87c5eca82c908ba82537a7091
Alex White <alexlloydwhite@gmail.com>
2021-05-14 16:34:32 -0500

    added app/toolbar with mui

--- src/components/App/Header.jsx
+++ src/components/App/Header.jsx
@@ -1,13 +1,32 @@
+// IMPORTS FOR REACT FRAMEWORKS!
 import { useHistory } from 'react-router-dom';
+// -------------------- MUI -----------------------
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import { Typography, makeStyles } from '@material-ui/core';
+import Button from '@material-ui/core/Button'
+
+const useStyles = makeStyles((theme) => {
+    return {
+        toolbarButton: {
+            marginLeft: 'auto'
+        }
+    }
+})
 
 const Header = () => {
     const history = useHistory();
+    const classes = useStyles();
     return (
-        <nav>
-            <h1>The Movies Saga!</h1>
-            <button onClick={() => history.push('/')}>home</button>
-            <button onClick={() => history.push('/addmovie')}>Add New Movie</button>
-        </nav>
+        <AppBar>
+            <Toolbar>
+                <Typography>The Movies Saga!</Typography>
+                <div className={classes.toolbarButton}>
+                    <Button onClick={() => history.push('/')}>home</Button>
+                    <Button onClick={() => history.push('/addmovie')}>Add New Movie</Button>
+                </div>
+            </Toolbar>
+        </AppBar>
     );
 }
 
