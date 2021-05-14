7ee76e163325a9a605eb1291faf62bf927cd5db6
Alex White <alexlloydwhite@gmail.com>
2021-05-13 20:15:34 -0500

    init form setup

--- src/components/AddMovieForm/AddMovieForm.jsx
+++ src/components/AddMovieForm/AddMovieForm.jsx
@@ -1,7 +1,48 @@
+import { useState } from "react";
+
 const AddMovieForm = () => {
-    return (  
-        <h5>Form to Add Movies</h5>
+    const [movieTitle, setMovieTitle] = useState('');
+    const [movieUrl, setMovieUrl] = useState('');
+    const [movieDescription, setMovieDescription] = useState('');
+    const [genre, setGenre] = useState('Genre 1')
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('clicked submit form');
+    }
+
+    return (
+        <div>
+            <h1>Add a New Movie!</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    value={movieTitle}
+                    placeholder="Movie Title"
+                    onChange={(e) => setMovieTitle(e.target.value)}
+                />
+                <input
+                    value={movieUrl}
+                    placeholder="Movie Url"
+                    onChange={(e) => setMovieUrl(e.target.value)}
+                />
+                <input
+                    value={movieDescription}
+                    placeholder="Movie Description"
+                    onChange={(e) => setMovieDescription(e.target.value)}
+                />
+                <select
+                    onChange={(e) => setGenre(e.target.value)}
+                >
+                    <option>Genre 1</option>
+                    <option>Genre 2</option>
+                    <option>Genre 3</option>
+                    <option>Genre 4</option>
+                    <option>Genre 5</option>
+                </select>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
     );
 }
- 
+
 export default AddMovieForm;
