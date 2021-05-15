2268ea946ad094a5a97aed7a63700291381f8855
Alex White <alexlloydwhite@gmail.com>
2021-05-14 20:39:16 -0500

    need to set inputs based on movie edit clicked

--- src/components/EditMovieForm/EditMovieForm.jsx
+++ src/components/EditMovieForm/EditMovieForm.jsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+
 const EditMovieForm = () => {
-    return (  
-        <h5>Edit Movie Form Here -- Under Construction!</h5>
+    const dispatch = useDispatch();
+    const params = useParams();
+
+    const [editTitle, setEditTitle] = useState('');
+    const [editDesc, setEditDesc] = useState('');
+
+    const editMovieDetail = useSelector(store => store.movieClicked)
+    console.log('movie to edit is:', editMovieDetail);
+
+    useEffect(() => {
+        dispatch({ type: 'SET_MOVIE_CLICK', payload: params.id })
+    }, [])
+
+    return (
+        <div>
+            <h5>Edit Movie Form Here -- Under Construction!</h5>
+        </div>
     );
 }
- 
+
 export default EditMovieForm;
