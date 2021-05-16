6a1d4de213cb8ce68eaa4994c1660359fa154d54
Alex White <alexlloydwhite@gmail.com>
2021-05-16 12:44:45 -0500

    search input sending data to server=>db=>client, woot

--- server/routes/search.router.js
+++ server/routes/search.router.js
@@ -5,6 +5,16 @@ const pool = require('../modules/pool');
 router.get('/:search', (req,res) => {
     let search = req.params.search;
     console.log('IN search router, searching for:', search);
+
+    const sqlQuery = `SELECT * FROM movies m
+                        WHERE UPPER ("title") LIKE UPPER('%' || $1 || '%');`;
+    pool.query(sqlQuery, [search])
+        .then(result => {
+            res.send(result.rows);
+        })
+        .catch(err => {
+            console.log(`IN search router, error making search ${search}: ${err}`);
+        })
 })
 
 module.exports = router;
